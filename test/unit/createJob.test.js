883e2279b03c8194a02304ddf9ecdab0de26d83b
Eugene Obrezkov <ghaiklor@gmail.com>
2015-08-13 17:10:27 +0300

    Write tests for createJob

---
+++ test/unit/createJob.test.js
@@ -0,0 +1,20 @@
+var assert = require('chai').assert;
+var createJob = require('../../lib/createJob');
+
+describe('sails-hook-cron::createJob', function () {
+  it('Should properly export', function () {
+    assert.isFunction(createJob);
+  });
+
+  it('Should properly create job', function () {
+    var job = createJob({
+      cronTime: '* * * * * *',
+      onTick: console.log.bind(console, 'onTick'),
+      onComplete: console.log.bind(console, 'onComplete'),
+      timezone: 'Europe/Kiev'
+    });
+
+    assert.isObject(job.cronTime);
+    assert.equal(job.cronTime.source, '* * * * * *');
+  });
+});
