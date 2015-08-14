31c93dbc98c6b37eb34399c20a5bfd01db1144ca
Eugene Obrezkov <ghaiklor@gmail.com>
2015-08-14 14:17:51 +0300

    Fix tests with equal schedule for different jobs

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -14,7 +14,7 @@ describe('sails-hook-cron::main', function () {
           onTick: console.log
         },
         secondJob: {
-          schedule: '* * * * * 2',
+          schedule: '* * * * * 1',
           onTick: console.log,
           onComplete: console.log,
           start: false,
@@ -57,7 +57,7 @@ describe('sails-hook-cron::main', function () {
     assert.ok(firstJob.running);
 
     assert.isFunction(secondJob.onComplete);
-    assert.equal(secondJob.cronTime.source, '* * * * * 2');
+    assert.equal(secondJob.cronTime.source, '* * * * * 1');
     assert.equal(secondJob.cronTime.zone, 'Europe/Kiev');
     assert.notOk(secondJob.running);
   });
